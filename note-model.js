0d30e2ae52fe436c9ef9b3f2342b63e20e914495
victorvallet <victorvallet8@gmail.com>
2020-06-22 15:53:27 +0200

    Added print() and pass test

--- note-model.js
+++ note-model.js
@@ -1,23 +1,30 @@
 var assert = {
-    isTrue: function(assertionToCheck) {
-      if (!assertionToCheck) {
-        throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
-      }
+  isTrue: function (assertionToCheck) {
+    if (!assertionToCheck) {
+      throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
     }
-  };
-
-  class Note{
-    constructor(text){
-      this.text = text 
+    else {
+      console.log("All good ✅")
     }
+  }
+};
+
+class Note {
+  constructor(text) {
+    this.text = text
+  }
+
+  print = () => {
+    return this.text
+  }
 
 
-  };
+};
 
-  // var note = new Note('I love JS!');
-  // console.log(note)
+// var note = new Note('I love JS!');
+// console.log(note.print())
 
-module.exports =  {
+module.exports = {
   Note,
   assert
 };
--- spec/note-model-test.js
+++ spec/note-model-test.js
@@ -1,10 +1,17 @@
 const functions = require('../note-model.js')
 
 
-function testNoteText(text){
-  const note = new functions.Note;
-  console.log(note)
+function testNoteText(text) {
+  const note = new functions.Note(text);
+  // console.log(note)
   functions.assert.isTrue(note.text === text);
 };
 
 testNoteText('I love JS!');
+
+function testNotePrint(text) {
+  const note = new functions.Note(text)
+  functions.assert.isTrue(note.print() === text);
+}
+
+testNotePrint('I love JS!');
